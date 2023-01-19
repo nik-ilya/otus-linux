507938b435fd8d60a4340644345a28b1525070ea
nik-ilya <105505171+nik-ilya@users.noreply.github.com>
2023-01-19 08:20:52 +0300

    Create 02-spawn-fcgi.sh

---
+++ 09-lesson/02-spawn-fcgi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sed -i 's/#SOCKET/SOCKET/g' /etc/sysconfig/spawn-fcgi
+sed -i 's/#OPTIONS/OPTIONS/g' /etc/sysconfig/spawn-fcgi
+
+
+cat > /etc/systemd/system/spawn-fcgi.service <<EOF
+[Unit]
+Description=Spawn-fcgi startup service by Otus
+After=network.target
+
+[Service]
+Type=simple
+PIDFile=/var/run/spawn-fcgi.pid
+EnvironmentFile=/etc/sysconfig/spawn-fcgi
+ExecStart=/usr/bin/spawn-fcgi -n \$OPTIONS
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable spawn-fcgi
+systemctl start spawn-fcgi
